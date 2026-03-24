bd0c655c93f76223e5bbb11786f4a542ac546a23
hindrashidi <har9509@nyu.edu>
2026-03-25 03:46:48 +0400

    Update project3.js

--- projects/project3/project3.js
+++ projects/project3/project3.js
@@ -1,9 +1,10 @@
-let step = 4; 
-let colorModeState = 0; // 0 = purple, 1 = blue, 2 = black/grey
+let step = 6;
+let colorModeState = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   pixelDensity(1);
+  frameRate(30);
 }
 
 function draw() {
@@ -14,21 +15,40 @@ function draw() {
 
   for (let y = 0; y < height; y += step) {
     for (let x = 0; x < width; x += step) {
-
       let dx = x - cx;
       let dy = y - cy;
 
       let d = sqrt(dx * dx + dy * dy);
       let a = atan2(dy, dx);
 
-      let mouseInfluence = mouseX * 0.001; 
+      let mouseInfluence = mouseX * 0.001;
+      let swirl = a + d * 0.05 + frameCount * 0.03 + mouseInfluence;
 
-      let swirl = a + d * 0.05 + frameCount * 0.05 + mouseInfluence;
-
-      // keep the blur / texture
       let v = 128 + 127 * sin(swirl);
-      v += random(-30, 30);
+      v += random(-12, 12);
       v = constrain(v, 0, 255);
+
+      let r, g, b;
+      let light = map(mouseY, 0, height, 0.7, 1.4);
+
+      if (colorModeState === 0) {
+        r = map(v, 0, 255, 40, 150) * light;
+        g = map(v, 0, 255, 10, 70) * light;
+        b = map(v, 0, 255, 70, 200) * light;
+      } else if (colorModeState === 1) {
+        r = map(v, 0, 255, 10, 60) * light;
+        g = map(v, 0, 255, 40, 140) * light;
+        b = map(v, 0, 255, 90, 255) * light;
+      } else {
+        let grey = map(v, 0, 255, 15, 170) * light;
+        r = grey;
+        g = grey;
+        b = grey;
+      }
+
+      r = constrain(r, 0, 255);
+      g = constrain(g, 0, 255);
+      b = constrain(b, 0, 255);
 
       for (let yy = 0; yy < step; yy++) {
         for (let xx = 0; xx < step; xx++) {
@@ -37,35 +57,9 @@ function draw() {
 
           if (px < width && py < height) {
             let i = (px + py * width) * 4;
-
-            let r, g, b;
-
-            // mouseY controls lightness
-            let light = map(mouseY, 0, height, 0.7, 1.4);
-
-            if (colorModeState === 0) {
-              // PURPLE: dark purple -> lighter purple
-              r = map(v, 0, 255, 40, 150) * light;
-              g = map(v, 0, 255, 10, 70) * light;
-              b = map(v, 0, 255, 70, 200) * light;
-            } 
-            else if (colorModeState === 1) {
-              // BLUE: dark blue -> lighter blue
-              r = map(v, 0, 255, 10, 60) * light;
-              g = map(v, 0, 255, 40, 140) * light;
-              b = map(v, 0, 255, 90, 255) * light;
-            } 
-            else {
-              // BLACK/GREY: black -> grey
-              let grey = map(v, 0, 255, 15, 170) * light;
-              r = grey;
-              g = grey;
-              b = grey;
-            }
-
-            pixels[i]     = constrain(r, 0, 255);
-            pixels[i + 1] = constrain(g, 0, 255);
-            pixels[i + 2] = constrain(b, 0, 255);
+            pixels[i] = r;
+            pixels[i + 1] = g;
+            pixels[i + 2] = b;
             pixels[i + 3] = 255;
           }
         }
